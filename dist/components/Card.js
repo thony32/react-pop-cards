96e3eb9edd893e9b13811f1f56bb95ab42ede32c
TiavinaCyri <tiavinacyri@gmail.com>
2024-01-21 17:29:02 +0400

    publish 0.1.8

--- dist/components/Card.js
+++ dist/components/Card.js
@@ -1,40 +1,48 @@
 "use strict";
 
+require("core-js/modules/es.symbol.description.js");
 require("core-js/modules/es.weak-map.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-require("core-js/modules/web.dom-collections.iterator.js");
 require("core-js/modules/es.array.reduce.js");
+require("core-js/modules/web.dom-collections.iterator.js");
 var _react = _interopRequireWildcard(require("react"));
 var _web = require("@react-spring/web");
 var _reactResponsive = require("react-responsive");
 require("../index.css");
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && Object.prototype.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable react-hooks/rules-of-hooks */
-
-const Card = () => {
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); } /* eslint-disable react-hooks/exhaustive-deps */ /* eslint-disable react-hooks/rules-of-hooks */
+const Card = _ref => {
+  let {
+    data,
+    bgColor
+  } = _ref;
   const isMobile = (0, _reactResponsive.useMediaQuery)({
     query: "max-width: 600px"
   });
   const activeCardRef = (0, _react.useRef)(null);
   const parentCard = (0, _react.useRef)(null);
 
-  // Define card states
+  // Define card data
   const initialCardDimensions = {
     width: "6rem",
     height: "6rem"
   };
-  const [cards, setCards] = (0, _react.useState)({
-    feu: initialCardDimensions,
-    water: initialCardDimensions,
-    wind: initialCardDimensions,
-    quake: initialCardDimensions
-  });
-  const [activeCard, setActiveCard] = (0, _react.useState)("feu");
+  const convertedObject = data.reduce((acc, currentValue) => {
+    acc[currentValue.toLowerCase()] = _objectSpread({}, initialCardDimensions);
+    return acc;
+  }, {});
+  const [cards, setCards] = (0, _react.useState)(convertedObject);
+
+  // active card
+  const [activeCard, setActiveCard] = (0, _react.useState)(data[0]);
 
   // Handle card click and update dimensions
   const handleCardClick = cardKey => {
@@ -96,7 +104,7 @@ const Card = () => {
     ref: activeCard === cardKey ? activeCardRef : null,
     style: animatedStyles[cardKey],
     onClick: () => handleCardClick(cardKey),
-    className: (activeCard === cardKey ? "px-6 py-4 rounded-2xl" : "flex justify-center items-center rounded-2xl") + " cursor-pointer bg-blue-500"
+    className: "".concat(activeCard === cardKey ? "px-6 py-4 rounded-2xl" : "flex justify-center items-center rounded-2xl", " cursor-pointer ").concat(bgColor)
   }, /*#__PURE__*/_react.default.createElement("div", {
     className: "space-y-3"
   }, /*#__PURE__*/_react.default.createElement("div", {
--- dist/index.js
+++ dist/index.js
@@ -11,7 +11,11 @@ Object.defineProperty(exports, "Card", {
 });
 var _react = _interopRequireDefault(require("react"));
 var _reactDom = _interopRequireDefault(require("react-dom"));
-var _Card = _interopRequireDefault(require("./components/Card"));
 require("./index.css");
+var _Card = _interopRequireDefault(require("./components/Card"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_Card.default, null)), document.getElementById('root'));
+const array = ["Un", "Deux", "Trois", "Quatre"];
+_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_Card.default, {
+  data: array,
+  bgColor: "bg-red-500"
+})), document.getElementById("root"));
