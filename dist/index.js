5808741889eecbc446cb2ed21248c8dd24ec264f
TiavinaCyri <tiavinacyri@gmail.com>
2024-01-21 16:46:56 +0400

    ao

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,9 @@ Object.defineProperty(exports, "Card", {
     return _Card.default;
   }
 });
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
 var _Card = _interopRequireDefault(require("./components/Card"));
 require("./index.css");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_Card.default, null)), document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from "./components/Card"
 import "./index.css"
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Card />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
 export { Card }
