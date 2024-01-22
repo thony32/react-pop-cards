598dec115d69881e878c8aa07ac683a580056d52
TiavinaCyri <tiavinacyri@gmail.com>
2024-01-22 18:12:19 +0400

    add disposition 1.0

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import Card from './components/Card';
 
-const array = ["Un","Deux","Trois","Quatre"]
+const array = ["Un", "Deux", "Trois", "Quatre"]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Card data={array} bgColor={""}/>
+        <Card data={array} />
     </React.StrictMode>,
 )
 
