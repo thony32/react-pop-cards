3a7cee5949c63029d3395ff0f99b785d3afe4505
TiavinaCyri <tiavinacyri@gmail.com>
2024-01-23 21:02:40 +0400

    sandox 1.0

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import Card from './components/Card';
+import Sandbox from "./Sandbox";
 
-const array = ["Un", "Deux", "Trois", "Quatre"]
+// const array = ["Un", "Deux", "Trois", "Quatre"]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Card data={array} />
+        {/* For sandbox */}
+        <Sandbox />
+        {/* For prod */}
+        {/* <Card data={array} /> */}
     </React.StrictMode>,
 )
 
