a83dc5c3758afc18deabe8d687c3ce576a818297
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-01-29 18:34:26 +0300

    [fixed]: build script

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,29 @@ var _Sandbox = _interopRequireDefault(require("./Sandbox"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 // import Card from './components/Card';
 
-// const array = ["Un", "Deux", "Trois", "Quatre"]
+// const array = [
+//     {
+//         title: "Title1",
+//         description: "Description1",
+//         image: "https://placehold.co/600x400",
+//     },
+//     {
+//         title: "Title2",
+//         description: "Description2",
+//         image: "https://placehold.co/600x400",
+//     },
+//     {
+//         title: "Title3",
+//         description: "Description3",
+//         image: "https://placehold.co/600x400",
+//     },
+//     {
+//         title: "Title4",
+//         description: "Description4",
+//         image: "https://placehold.co/600x400",
+//     },
+// ]
 
-_client.default.createRoot(document.getElementById('root')).render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_Sandbox.default, null)));
+_client.default.createRoot(document.getElementById("root")).render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_Sandbox.default, null)));
 
 // export { Card }
