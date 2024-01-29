ee3854e53f998a4f7d039fe1a74778191fa55c94
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-01-29 19:10:01 +0300

    vercel

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import Card from "./components/Card"
-// import Sandbox from "./Sandbox"
+// import Card from "./components/Card"
+import Sandbox from "./Sandbox"
 
-const array = [
-    {
-        title: "Title1",
-        description: "Description1",
-        image: "https://placehold.co/600x400",
-    },
-    {
-        title: "Title2",
-        description: "Description2",
-        image: "https://placehold.co/600x400",
-    },
-    {
-        title: "Title3",
-        description: "Description3",
-        image: "https://placehold.co/600x400",
-    },
-    {
-        title: "Title4",
-        description: "Description4",
-        image: "https://placehold.co/600x400",
-    },
-]
+// const array = [
+//     {
+//         title: "Title1",
+//         description: "Description1",
+//         image: "https://placehold.co/600x400",
+//     },
+//     {
+//         title: "Title2",
+//         description: "Description2",
+//         image: "https://placehold.co/600x400",
+//     },
+//     {
+//         title: "Title3",
+//         description: "Description3",
+//         image: "https://placehold.co/600x400",
+//     },
+//     {
+//         title: "Title4",
+//         description: "Description4",
+//         image: "https://placehold.co/600x400",
+//     },
+// ]
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         {/* For sandbox */}
-        {/* <Sandbox /> */}
+        <Sandbox />
         {/* For prod */}
-        <Card data={array} disposition="LeftRight" isRounded={false} tension={120} friction={10} bgColor="#e5e7eb" />
+        {/* <Card data={array} disposition="LeftRight" isRounded={false} tension={120} friction={10} bgColor="#e5e7eb" /> */}
     </React.StrictMode>
 )
 
