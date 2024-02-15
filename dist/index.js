ba9d3926a907e1e518166bd36075c3478aeaf142
TiavinaCyri <tiavinacyri@gmail.com>
2024-02-15 18:54:06 +0400

    merge

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,14 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Card", {
+  enumerable: true,
+  get: function get() {
+    return _Card.default;
+  }
+});
 var _react = _interopRequireDefault(require("react"));
 var _client = _interopRequireDefault(require("react-dom/client"));
 require("./index.css");
