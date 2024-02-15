0438f99b960a40ab5cea4c212d0275924d6f9255
TiavinaCyri <tiavinacyri@gmail.com>
2024-02-15 18:53:56 +0400

    merge

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,18 +1,35 @@
+<<<<<<< HEAD
 import { ReactNode, ReactElement, CSSProperties } from 'react';
 
+=======
+>>>>>>> 2d59d5f (merge)
 interface CardDataItem {
   title: string;
   description: string;
   image?: string;
+<<<<<<< HEAD
+=======
+  // Ajoutez d'autres propriétés si nécessaire
+>>>>>>> 2d59d5f (merge)
 }
 
 interface CardProps {
   data: CardDataItem[];
   bgColor?: string;
+<<<<<<< HEAD
   disposition?: 'LeftRight' | 'RightLeft' | 'TopBottom' | 'BottomTop';
+=======
+  disposition?: "LeftRight" | "RightLeft" | "TopBottom" | "BottomTop";
+>>>>>>> 2d59d5f (merge)
   isRounded?: boolean;
   tension?: number;
   friction?: number;
 }
 
+<<<<<<< HEAD
 export declare const Card: React.FC<CardProps>;
+=======
+declare const Card: React.FC<CardProps>;
+
+export default Card;
+>>>>>>> 2d59d5f (merge)
